8abdf47ca90f58e91f2ac51007e9dcab7670aa91
jpaslk <pasindu.jayasekara@ifs.com>
2025-10-18 18:14:09 +0530

    update backend settings

--- src/assets/components/layout/settings.tsx
+++ src/assets/components/layout/settings.tsx
@@ -4,7 +4,7 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 const Settings: React.FC = () => {
-  const [apiUrl, setApiUrl] = useState<string>("http://localhost:5000");
+  const [apiUrl, setApiUrl] = useState<string>(`http://${window.location.hostname}:5000`);
   const navigate = useNavigate();
 
   useEffect(() => {
